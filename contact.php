edf3abc40ca5e79bf98f4423dab8566564fce19c
Johannes Loewe <johannes@loewe.pm>
2018-03-12 19:00:02 +0100

    Change recipient email adress

--- contact.php
+++ contact.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "johannes@loewe.pm";
+        $recipient = "kontakt@feuertruppe.de";
 
         // Set the email subject.
         $subject = "New contact from $name";
